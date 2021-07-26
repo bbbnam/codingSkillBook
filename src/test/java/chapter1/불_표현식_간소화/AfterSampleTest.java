c28844e2764e4558820db21c06f9d429b1bb349e
bbbnam <bbbnam@naver.com>
2021-07-26 23:26:17 +0900

    test : 불 표현식 간소화 테스트

---
+++ src/test/java/chapter1/불_표현식_간소화/AfterSampleTest.java
@@ -0,0 +1,17 @@
+package chapter1.불_표현식_간소화;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AfterSampleTest {
+
+    @DisplayName("missionCount가 0보다 작거나 이름값이 비어있으면, 유효성 검사가 실패(false)한다.")
+    @Test
+    void isValid() {
+        AfterSample afterSample = new AfterSample("대충지은이름", -1);
+
+        assertThat(afterSample.isValid()).isFalse();
+    }
+}
