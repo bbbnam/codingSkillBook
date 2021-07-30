0be4a31c8505aaf67f22d3d85f2db253c7bca092
bbbnam <bbbnam@naver.com>
2021-07-30 23:28:12 +0900

    test : 항상 괄호 사용하기 테스트

---
+++ src/test/java/chapter1/항상_괄호_사용하기/AfterSampleTest.java
@@ -0,0 +1,13 @@
+package chapter1.항상_괄호_사용하기;
+
+import org.junit.jupiter.api.Test;
+
+class AfterSampleTest {
+
+    @Test
+    void check() {
+        AfterSample afterSample = new AfterSample();
+
+        afterSample.check();
+    }
+}
---
+++ src/test/java/chapter1/항상_괄호_사용하기/BeforeSampleTest.java
@@ -0,0 +1,13 @@
+package chapter1.항상_괄호_사용하기;
+
+import org.junit.jupiter.api.Test;
+
+class BeforeSampleTest {
+
+    @Test
+    void check() {
+        BeforeSample beforeSample = new BeforeSample();
+
+        beforeSample.check();
+    }
+}
